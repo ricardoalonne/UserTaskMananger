7c3ec9083b6138eccb7efa22bfe2ee2ec62bacaf
ricardoalonne <ricardo.alonne@gmail.com>
2024-01-12 11:47:14 -0500

    feat: user resume response model created.

---
+++ Model/UserTaskMananger.DTOs/Response/UserResumeResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserTaskMananger.Entities;
+
+namespace UserTaskMananger.DTOs.Response
+{
+    public class UserResumeResponse
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public UserResumeResponse()
+        {
+
+        }
+
+        public UserResumeResponse(User entity)
+        {
+            this.Id = entity.Id;
+            this.FullName = $"{entity.Name} {entity.LastName}";
+        }
+    }
+}
