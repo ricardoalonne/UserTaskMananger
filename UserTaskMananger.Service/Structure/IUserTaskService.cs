2e5b62954a7c15b7c10cf4240cba559f0ec54643
ricardoalonne <ricardo.alonne@gmail.com>
2024-01-12 11:52:05 -0500

    feat: added new features to the user task service.

--- UserTaskMananger.Service/Structure/IUserTaskService.cs
+++ UserTaskMananger.Service/Structure/IUserTaskService.cs
@@ -1,10 +1,14 @@
 ﻿using UserTaskMananger.DTOs.Request;
 using UserTaskMananger.DTOs.Response;
+using UserTaskMananger.Entities;
 using UserTaskMananger.Service.Base;
 
 namespace UserTaskMananger.Service.Structure
 {
     public interface IUserTaskService : IServiceBase<UserTaskResponse, UserTaskRequest>
     {
+        Task<IEnumerable<UserTaskResponse>> GetByUserIdAndMode(int userId, string mode);
+        Task<bool> Remove(int id);
+        Task<bool> Finish(int id);
     }
 }
